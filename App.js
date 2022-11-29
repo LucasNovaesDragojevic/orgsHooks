7e44136cd19761345148e4b71993fb9c6da7a3c4
Lucas Novae Dragojevic <lucas.dragojevic@gmail.com>
2022-11-29 19:43:23 -0300

    Initial structure

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+export default function App() {
+  return <SafeAreaView> 
+          <Text>Olá mundo</Text> 
+        </SafeAreaView>
+}
